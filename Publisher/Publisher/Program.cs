da77a41cc1a7f4f9868290851c6e142d2e301d10
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-10-18 18:27:47 -0300

    first commit

---
+++ Publisher/Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saldacao_1",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "Bem-vindo ao RabbitMQ";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "saldacao_1",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($"[X] Enviada: {message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
